9b14571f825111c80cf8622bd4ad7ececd7c422e
aagraziani <aagraziani@gmail.com>
2020-05-13 18:37:51 -0300

    arreglamos header cliente

--- fastenWeb/controllers/registerController.js
+++ fastenWeb/controllers/registerController.js
@@ -1,10 +1,20 @@
+const fs = require('fs');
+
 let controlador = {
     register: function(req, res){
         res.render('register');
     },
     registerUser: function(req, res){
+        
+        let usuario = {
+            nombre: req.body.nombre,
+            apellido: req.body.apellido,
+            email: req.body.email,
+            password: password, 
+        }
+        
         res.redirect('/');
-    }
+    },
 };
 
 module.exports = controlador;
