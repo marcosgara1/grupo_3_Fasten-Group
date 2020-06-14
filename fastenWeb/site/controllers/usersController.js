230211444212b1befedb886fe3ebf37bd9365f8e
aagraziani <aagraziani@gmail.com>
2020-06-14 18:16:45 -0300

    agregamos registro de usuario y login

---
+++ fastenWeb/site/controllers/usersController.js
@@ -0,0 +1,48 @@
+const userData = require('../models/user');
+const bcrypt = require('bcrypt');
+
+let controlador = {
+
+    formLogin : function (req, res) {
+        
+        res.render('formularioLogin');
+    },
+
+    processLogin : function (req, res) {
+        
+        let users = userData.findAll();
+
+        for (let i = 0; i < users.length; i++){
+            
+            if (req.body.email == users[i].email && bcrypt.compareSync(req.body.password, users[i].password)){
+                res.redirect('/')
+            }
+        }
+
+        res.send('Ups, pasaron cosas');
+    },
+
+    formRegister : function (req, res) {
+       
+        res.render('formularioRegistro');
+    },
+
+    register : function (req, res) {
+
+        let user = {
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password, 10),
+            foto : req.files[0].filename
+        }
+
+        userData.create(user);
+
+        res.redirect('/profile', { user : user });
+    }
+
+    
+};
+
+module.exports = controlador;
--- fastenWeb/site/routes/users.js
+++ fastenWeb/site/routes/users.js
@@ -1,9 +1,28 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const multer = require('multer');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'public/img/users');
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+  })
+   
+  const upload = multer({ storage: storage });
+
+const usersController = require('../controllers/usersController');
+
+
+router.get('/login', usersController.formLogin);
+
+router.post('/login', usersController.processLogin)
+
+router.get('/register', usersController.formRegister);
+
+router.post('/', upload.any('foto'),usersController.register);
 
 module.exports = router;
