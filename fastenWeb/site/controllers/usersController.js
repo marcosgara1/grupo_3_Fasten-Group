28d9b8b470bc020d4be081ad38b297fca4ae684c
aagraziani <aagraziani@gmail.com>
2020-06-14 21:48:24 -0300

    validaciones formulario de registro

--- fastenWeb/site/controllers/usersController.js
+++ fastenWeb/site/controllers/usersController.js
@@ -1,20 +1,21 @@
 const userData = require('../models/user');
 const bcrypt = require('bcrypt');
+const { check, validationResult, body } = require('express-validator');
 
 let controlador = {
 
-    formLogin : function (req, res) {
-        
+    formLogin: function (req, res) {
+
         res.render('formularioLogin');
     },
 
-    processLogin : function (req, res) {
-        
+    processLogin: function (req, res) {
+
         let users = userData.findAll();
 
-        for (let i = 0; i < users.length; i++){
-            
-            if (req.body.email == users[i].email && bcrypt.compareSync(req.body.password, users[i].password)){
+        for (let i = 0; i < users.length; i++) {
+
+            if (req.body.email == users[i].email && bcrypt.compareSync(req.body.password, users[i].password)) {
                 res.redirect('/')
             }
         }
@@ -22,27 +23,37 @@ let controlador = {
         res.redirect('/users/login');
     },
 
-    formRegister : function (req, res) {
-       
-        res.render('formularioRegistro');
+    formRegister: function (req, res) {
+
+        res.render('register');
     },
 
-    register : function (req, res) {
+    register: function (req, res) {
 
-        let user = {
-            first_name: req.body.first_name,
-            last_name: req.body.last_name,
-            email: req.body.email,
-            password: bcrypt.hashSync(req.body.password, 10),
-            foto : req.files[0].filename
+        let errors = validationResult(req);
+
+        if (errors.isEmpty()) {
+
+            let user = {
+                first_name: req.body.first_name,
+                last_name: req.body.last_name,
+                email: req.body.email,
+                password: bcrypt.hashSync(req.body.password, 10),
+                foto: req.files[0].filename
+            }
+
+            userData.create(user);
+
+            res.redirect('products'/*, { user : user }*/);
+
+        } else {
+
+            res.render('register', { errors: errors.errors });
+
         }
-
-        userData.create(user);
-
-        res.redirect('products'/*, { user : user }*/);
     }
 
-    
+
 };
 
 module.exports = controlador;
--- fastenWeb/site/routes/users.js
+++ fastenWeb/site/routes/users.js
@@ -2,17 +2,21 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const multer = require('multer');
+const { check, validationResult, body } = require('express-validator');
+const fs = require('fs');
+const userData = require('../models/user');
+
 
 var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'public/img/users');
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-    }
-  })
-   
-  const upload = multer({ storage: storage });
+  destination: function (req, file, cb) {
+    cb(null, 'public/img/users');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+})
+
+const upload = multer({ storage: storage });
 
 const usersController = require('../controllers/usersController');
 
@@ -23,6 +27,34 @@ router.post('/login', usersController.processLogin)
 
 router.get('/register', usersController.formRegister);
 
-router.post('/', upload.any('foto'),usersController.register);
+router.post('/', upload.any('foto'), [
+
+  check('first_name').isLength({ min: 1 }).withMessage('Este campo debe estar completo'),
+
+  check('last_name').isLength({ min: 1 }).withMessage('Este campo debe estar completo'),
+
+  check('email').isEmail().withMessage('Debe ingresar un email válido'),
+
+  check('password').isLength({ min: 8 }).withMessage('La contraseña debe poseer al menos 8 caracteres'),
+
+  body('email').custom(function (value) {
+
+    let users = userData.findAll();
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].email == value) {
+        return false;
+      }
+    }
+    return true;
+  }).withMessage('El email ya está registrado'),
+
+  body('c_password').custom(function(value){
+    if ('password' != value){
+      return false;
+    }
+    return true;
+  }).withMessage('La contraseña debe coincidir con la ingresada anteriormente')
+
+], usersController.register);
 
 module.exports = router;
